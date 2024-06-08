670117265166443a42395c586179547f59a35eca
SudhanshuSingh6 <sudhanshus026@gmail.com>
2024-06-08 14:11:53 +0530

    register and login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGOURI as string);
 const app = express();
@@ -9,9 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "hello" });
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(4000, () => {
   console.log("server started");
