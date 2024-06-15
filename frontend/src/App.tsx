ef3141ff0c7e82e6bf828f6bda00d607c70d40ca
SudhanshuSingh6 <sudhanshus026@gmail.com>
2024-06-15 23:30:40 +0530

    sign-in page and middleware

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,8 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
-import Register from "./pages/Register"
+import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
 
 const App = () => {
   return (
@@ -32,6 +33,14 @@ const App = () => {
           element={
             <Layout>
               <Register />
+            </Layout>
+          }
+        />
+        <Route
+          path="/sign-in"
+          element={
+            <Layout>
+              <SignIn />
             </Layout>
           }
         />
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 export type RegisterFormData = {
   firstName: string;
   lastName: string;
@@ -9,6 +11,8 @@ export type RegisterFormData = {
   confirmPassword: string;
 };
 const Register = () => {
+  const navigate = useNavigate();
+  const { showToast } = useAppContext();
   const {
     register,
     watch,
@@ -17,10 +21,11 @@ const Register = () => {
   } = useForm<RegisterFormData>();
   const mutation = useMutation(apiClient.register, {
     onSuccess: () => {
-      console.log("registration succesfull");
+      showToast({ message: "Registration Sucess!", type: "SUCCESS" });
+      navigate("/");
     },
     onError: (error: Error) => {
-      console.log(error.message);
+      showToast({ message: error.message, type: "ERROR" });
     },
   });
   const onSubmit = handleSubmit((data) => {
